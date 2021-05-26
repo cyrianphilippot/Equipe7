65fff0922ee40974e5d379084d0c261acb5755df
cicibonbec <cyrian.philippot@ynov.com>
2021-05-26 16:28:53 +0200

    correctif d'erreur

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 
 Notre projet sql a pour thème l'école.
 
-- Pour la contruire ta base de donnée : [doc spécifique](./sripts/crea-base.sql)
-- Pour le remplissage de ta base de donnée : [doc spécifique](./scripts/filling-base.sql)
-- Pour le requêter ta base de donnée : [doc spécifique](./scripts/request.sql)
+- Pour la contruire ta base de donnée : [Création](./blob/main/scritps/crea-base.sql)
+- Pour le remplissage de ta base de donnée : [Remplissage](./blob/main/scritps/filling-base.sql)
+- Pour le requêter ta base de donnée : [Requêtes](./blob/main/scritps/request.sql)
 
 Orchester par Mathieu-Malory-Léa-Cyrian
--- scritps/request.sql
+++ scritps/request.sql
@@ -1,5 +1,6 @@
 use Equipe7
 
+/*
 SELECT * FROM Teachers
 
 SELECT * FROM Teaching_details
@@ -15,3 +16,39 @@ SELECT * FROM Classrooms
 SELECT * FROM Student_classes
 
 SELECT * FROM Students
+*/
+
+------------------------------- CURSOR -------------------------------
+-- Quelle est la liste de tout les prénoms de l'ensemble des élèves
+BEGIN TRAN
+
+DECLARE @firstname varchar(10)
+
+DECLARE curseur_students CURSOR FOR (SELECT firstname FROM Students)
+OPEN curseur_students
+FETCH curseur_students INTO @firstname
+
+WHILE @@FETCH_STATUS = 0
+
+BEGIN 
+	PRINT @firstname
+	FETCH NEXT FROM curseur_students INTO @firstname
+END
+CLOSE curseur_students
+DEALLOCATE curseur_students 
+
+ROLLBACK TRAN
+
+
+
+------------------------------- AUTRE -------------------------------
+-- Top 10 des noms des élèves les plus longs.
+SELECT TOP 10 (LEN(lastname)) AS '+ letters/name', lastname, student_id
+FROM Students 
+ORDER BY LEN(lastname) DESC
+
+
+-- Y'a t-il des cours qui n'ont pas de salle de classe ?
+SELECT subject_id, name AS 'name of classe' 
+FROM Classes, Classrooms 
+WHERE lower(name) LIKE 'null'
